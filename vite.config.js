7ba5c6294a3b58410097c21e6b58b88e6287df6e
Ainsley-1 <149393682+Ainsley-1@users.noreply.github.com>
2025-11-21 11:38:50 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react({
+    include: ['**/*.jsx', '**/*.js']
+  })],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+})
