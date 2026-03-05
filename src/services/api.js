d0afa32b6395a4f34ec048ae1674bc3dbc9198c8
Ainsley-1 <149393682+Ainsley-1@users.noreply.github.com>
2026-03-05 18:04:51 +0300

    Refactor API service to use environment variable for base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 ﻿import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
@@ -81,5 +83,6 @@ export const ordersService = {
 };
 
 export default api;
+
 
 
