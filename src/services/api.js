e537d3a0bf75f3992350424d39b34d8b173aac6d
Ainsley-1 <149393682+Ainsley-1@users.noreply.github.com>
2026-03-05 18:32:34 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_BASE,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -83,6 +83,7 @@ export const ordersService = {
 };
 
 export default api;
+
 
 
 
